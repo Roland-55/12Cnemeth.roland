928da251f3075a8e1d44fb7481855e6e14122e88
Roland-55 <nemeth.roland@wm-iskola.hu>
2021-09-20 09:38:29 +0200

    3. feladat

--- 2021.09.20/2021.09.20/Program.cs
+++ 2021.09.20/2021.09.20/Program.cs
@@ -11,17 +11,17 @@ namespace _2021._09._20
         static void Main(string[] args)
         {
             int szam=Convert.ToInt32(Console.ReadLine());
-            if (szam % 2==0) 
+            if (szam % 2<0) 
 
            
             {
-                Console.WriteLine("A szám pozitív");
+                Console.WriteLine("A hőmérséklet fagy");
             }
 
             else
 
             {
-                Console.WriteLine("A szám negatív");
+                Console.WriteLine("A hőmérséklet nem fagy");
             }
 
 
