c51e040d4e2d264d7b99e6e66da765a739a58158
Roland-55 <nemeth.roland@wm-iskola.hu>
2021-10-04 09:36:17 +0200

    1. feladat

---
+++ 20211004/20211004/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _20211004
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            {
+             for (int i = -30; i <= 30; i = i + 5)
+             {
+                 Console.WriteLine(i);
+
+             }
+                 Console.ReadLine(); 
+
+         }
+
+            int i = -30;
+            while (i <= 30)
+            {
+                Console.WriteLine(i);
+                i=i + 5;
+            }
+            Console.ReadLine();
+        }
+    } }
